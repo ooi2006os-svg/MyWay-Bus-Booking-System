90553283afedaa7f5ba0a36e967b0acdfbba8ae2
ooi2006os-svg <ooi2006.os@gmail.com>
2025-09-26 00:33:02 +0800

    Update MyWay V2.cpp

--- MyWay1/MyWay1.cpp
+++ MyWay1/MyWay1.cpp
@@ -1,141 +1,240 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
-int main() {
-    const std::string USER = "customer";
-    const std::string PASS = "12345";
+int main()
+{
+    string username, MMU_CyberJaya,DpulzeMallX, Apple_Store, Mamak_Power, IOI_CyberJaya;
+    float password, p_station, d_station, total_price, price_per_station, shift;
 
-    bool loggedIn = false;
+    password = 123456;
+    total_price = 0;
+    price_per_station = 1.50;
 
-    while (true) {
-        if (!loggedIn) {
-        login:
-            system("cls");
-            std::cout << "MyWay Bus Booking System\n";
-            std::cout << "1. Login\n2. Exit\n";
-            std::cout << "\nSelect option: ";
+    cout << "Enter your username: ";
+    getline(cin, username);
+    cout << "Enter your password: ";
+    cin >> password;
+        if (password == 123456)
+            {
+                cout << "Welcome " << username << "!" << endl;
+                cout << "Bus routines are as follows: " << endl;
+                cout << " MMU CyberJaya -> DpulzeMallX -> Apple Store -> Mamak Power -> IOI CyberJaya -> MMU Cyberjaya" << endl;
+                cout << "----------------------------------------------------------------------------------------------" << endl;
+                cout << "Please select your pickup station: " << endl;
+                cout << "1. MMU_CyberJaya " << "Shift available: 7:00 AM, 10:00 AM, 1:00 PM, 4:00 PM" << endl;
+                cout << "2. DpulzeMallX " << "Shift available: 7:15 AM, 10:15 AM, 1:15 PM, 4:15 PM" << endl;
+                cout << "3. Apple_Store " << "Shift available: 7:30 AM, 10:30 AM, 1:30 PM, 4:30 PM" << endl;
+                cout << "4. Mamak_Power " << "Shift available: 7:45 AM, 10:45 AM, 1:45 PM, 4:45 PM" << endl;
+                cout << "5. IOI_CyberJaya " << "Shift available: 8:00 AM, 11:00 AM, 2:00 PM, 5:00 PM" << endl;
+                cin >> p_station;
+                if (p_station == 1)
+                {
+                    cout << "Please select your shift: " << endl;
+                    cout << "1. 7:00 AM" << endl;
+                    cout << "2. 10:00 AM" << endl;
+                    cout << "3. 1:00 PM" << endl;
+                    cout << "4. 4:00 PM" << endl;
+                    cin >> shift;
+                    cout << "Please select your destination station: " << endl;
+                    cout << "1. DpulzeMallX" << endl;
+                    cout << "2. Apple_Store" << endl;
+                    cout << "3. Mamak_Power" << endl;
+                    cout << "4. IOI_CyberJaya" << endl;
+                    cin >> d_station;
+                    if (d_station == 1)
+                    {
+                        total_price = price_per_station * 1;
+                        cout << "The total price is RM" << total_price << endl;
+                        return 0;
+                    }
+                    if (d_station == 2)
+                    {
+                        total_price = price_per_station * 2;
+                        cout << "The total price is RM" << total_price << endl;
+                        return 0;
+                    }
+                    if (d_station == 3)
+                    {
+                        total_price = price_per_station * 3;
+                        cout << "The total price is RM" << total_price << endl;
+                        return 0;
+                    }
+                    if (d_station == 4)
+                    {
+                        total_price = price_per_station * 4;
+                        cout << "The total price is RM" << total_price << endl;
+                        return 0;
+                    }
 
-            int choice;
-            if (!(std::cin >> choice)) {
-                std::cin.clear();
-                std::cin.ignore(1000, '\n');
-                system("pause");
-                std::cout << "Invalid input. Please enter a number.\n";
-                goto login;
-            }
-
-            if (choice == 1) {
-                std::string username, password;
-                system("cls");
-                std::cout << "Username: "; std::cin >> username;
-                std::cout << "Password: "; std::cin >> password;
-
-                if (username == USER && password == PASS) {
-                    loggedIn = true;
-                    std::cout << "Login successful. Welcome, " << USER << "!\n";
                 }
-                else {
-                    std::cout << "\nLogin failed. Please Try Again\n\n";
-                    system("pause");
-                    goto login;
+                if (p_station == 2)
+                {
+                    cout << "Please select your shift: " << endl;
+                    cout << "1. 7:15 AM" << endl;
+                    cout << "2. 10:15 AM" << endl;
+                    cout << "3. 1:15 PM" << endl;
+                    cout << "4. 4:15 PM" << endl;
+                    cin >> shift;
+                    cout << "Please select your destination station: " << endl;
+                    cout << "1. Apple_Store" << endl;
+                    cout << "2. Mamak_Power" << endl;
+                    cout << "3. IOI_CyberJaya" << endl;
+                    cout << "4. MMU_CyberJaya" << endl;
+                    cin >> d_station;
+                    if (d_station == 1)
+                    {
+                        total_price = price_per_station * 1;
+                        cout << "The total price is RM" << total_price << endl;
+                        return 0;
+                    }
+                    if (d_station == 2)
+                    {
+                        total_price = price_per_station * 2;
+                        cout << "The total price is RM" << total_price << endl;
+                        return 0;
+                    }
+                    if (d_station == 3)
+                    {
+                        total_price = price_per_station * 3;
+                        cout << "The total price is RM" << total_price << endl;
+                        return 0;
+                    }
+                    if (d_station == 4)
+                    {
+                        total_price = price_per_station * 4;
+                        cout << "The total price is RM" << total_price << endl;
+                        return 0;
+                    }
                 }
+                if (p_station == 3)
+                {
+                    cout << "Please select your shift: " << endl;
+                    cout << "1. 7:30 AM" << endl;
+                    cout << "2. 10:30 AM" << endl;
+                    cout << "3. 1:30 PM" << endl;
+                    cout << "4. 4:30 PM" << endl;
+                    cin >> shift;
+                    cout << "Please select your destination station: " << endl;
+                    cout << "1. Mamak_Power" << endl;
+                    cout << "2. IOI_CyberJaya" << endl;
+                    cout << "3. MMU_CyberJaya" << endl;
+                    cout << "4. DpulzeMallX" << endl;
+                    cin >> d_station;
+                    if (d_station == 1)
+                    {
+                        total_price = price_per_station * 1;
+                        cout << "The total price is RM" << total_price << endl;
+                        return 0;
+                    }
+                    if (d_station == 2)
+                    {
+                        total_price = price_per_station * 2;
+                        cout << "The total price is RM" << total_price << endl;
+                        return 0;
+                    }
+                    if (d_station == 3)
+                    {
+                        total_price = price_per_station * 3;
+                        cout << "The total price is RM" << total_price << endl;
+                        return 0;
+                    }
+                    if (d_station == 4)
+                    {
+                        total_price = price_per_station * 4;
+                        cout << "The total price is RM" << total_price << endl;
+                        return 0;
+                    }
+                }
+                if (p_station == 4)
+                {
+                    cout << "Please select your shift: " << endl;
+                    cout << "1. 7:45 AM" << endl;
+                    cout << "2. 10:45 AM" << endl;
+                    cout << "3. 1:45 PM" << endl;
+                    cout << "4. 4:45 PM" << endl;
+                    cin >> shift;
+                    cout << "Please select your destination station: " << endl;
+                    cout << "1. IOI_CyberJaya" << endl;
+                    cout << "2. MMU_CyberJaya" << endl;
+                    cout << "3. DpulzeMallX" << endl;
+                    cout << "4. Apple_Store" << endl;
+                    cin >> d_station;
+                    if (d_station == 1)
+                    {
+                        total_price = price_per_station * 1;
+                        cout << "The total price is RM" << total_price << endl;
+                        return 0;
+                    }
+                    if (d_station == 2)
+                    {
+                        total_price = price_per_station * 2;
+                        cout << "The total price is RM" << total_price << endl;
+                        return 0;
+                    }
+                    if (d_station == 3)
+                    {
+                        total_price = price_per_station * 3;
+                        cout << "The total price is RM" << total_price << endl;
+                        return 0;
+                    }
+                    if (d_station == 4)
+                    {
+                        total_price = price_per_station * 4;
+                        cout << "The total price is RM" << total_price << endl;
+                        return 0;
+                    }
+                }
+                if (p_station == 5)
+                {
+                    cout << "Please select your shift: " << endl;
+                    cout << "1. 8:00 AM" << endl;
+                    cout << "2. 11:00 AM" << endl;
+                    cout << "3. 2:00 PM" << endl;
+                    cout << "4. 5:00 PM" << endl;
+                    cin >> shift;
+                    cout << "Please select your destination station: " << endl;
+                    cout << "1. MMU_CyberJaya" << endl;
+                    cout << "2. DpulzeMallX" << endl;
+                    cout << "3. Apple_Store" << endl;
+                    cout << "4. Mamak_Power" << endl;
+                    cin >> d_station;
+                    if (d_station == 1)
+                    {
+                        total_price = price_per_station * 1;
+                        cout << "The total price is RM" << total_price << endl;
+                        return 0;
+                    }
+                    if (d_station == 2)
+                    {
+                        total_price = price_per_station * 2;
+                        cout << "The total price is RM" << total_price << endl;
+                        return 0;
+                    }
+                    if (d_station == 3)
+                    {
+                        total_price = price_per_station * 3;
+                        cout << "The total price is RM" << total_price << endl;
+                        return 0;
+                    }
+                    if (d_station == 4)
+                    {
+                        total_price = price_per_station * 4;
+                        cout << "The total price is RM" << total_price << endl;
+                        return 0;
+                    }
+                }
+                else
+                {
+                    cout << "Invalid station. Please try again." << endl;
+                    return 0;
+                }
+
             }
-            else if (choice == 2) {
-                break;
-            }
-            else {
-                std::cout << "Unknown selection.\n";
-            }
+        else
+        {
+            cout << "Invalid password. Please try again." << endl;
+            return 0;
         }
-        else {
-            system("pause");
-            system("cls");
-            std::cout << "MyWay Bus Booking System\n";
-            std::cout << "1. Make Your Boooking \n2. Update Booking\n3. View Booking\n4. Delete Booking\n5. Logout\n6. Exit\n";
-            std::cout << "\nSelect option: ";
 
-            int choice2;
-            if (!(std::cin >> choice2)) {
-                std::cin.clear();
-                std::cin.ignore(1000, '\n');
-                system("pause");
-                std::cout << "Invalid input. Please enter a number.\n";
-                continue;
-            }
-
-            int pickup;
-            int dropoff;
-
-            std::string locations[] = { "", "DPulzeMallX", "Apple Store", "Mamak Power" };
-
-            if (choice2 == 1) {
-                booking:
-                system("cls");
-                std::cout << "Please Choose Your Pickup Location: \n";
-                std::cout << "1. DPulzeMallX \n2. Apple Store \n3. Mamak Power\n";
-                std::cout << "\nSelect option: ";
-
-                if (!(std::cin >> pickup)) {
-                    std::cin.clear();
-                    std::cin.ignore(1000, '\n');
-                    system("pause");
-                    std::cout << "Invalid input. Please enter a number.\n";
-                    continue;
-                }
-
-                std::cout << "\n\nPlease Choose Your DropOff Location: \n";
-                std::cout << "1. DPulzeMallX \n2. Apple Store \n3. Mamak Power\n";
-                std::cout << "\nSelect option: ";
-
-                if (!(std::cin >> dropoff)) {
-                    std::cin.clear();
-                    std::cin.ignore(1000, '\n');
-                    system("pause");
-                    std::cout << "Invalid input. Please enter a number.\n";
-                    continue;
-                }
-
-                if (pickup < 1 || pickup > 3 || dropoff < 1 || dropoff > 3) {
-                    std::cout << "Invalid selection.\n";
-                    continue;
-                }
-
-                if (pickup == dropoff) {
-                    std::cout << "\nPickup and DropOff cannot be the same location. Please Try Again\n";
-                    system("pause");
-                    goto booking;
-                }
-
-                std::cout << "\n\nYou Have Made A Booking To PickUp From: "
-                    << locations[pickup]
-                    << " And DropOff At: "
-                    << locations[dropoff]
-                    << "\n";
-
-
-            }
-
-            else if (choice2 == 2) {
-
-            }
-            else if (choice2 == 3) {
-
-            }
-            else if (choice2 == 4) {
-
-            }
-            else if (choice2 == 5) {
-                loggedIn = false;
-                std::cout << "Logged out.\n";
-            }
-            else if (choice2 == 6) {
-                break;
-            }
-            else {
-                std::cout << "Unknown command.\n";
-            }
-        }
-    }
-
-    return 0;
 }
